1cb9d2a54f843469c5a8948ce048a09fdc4f7b06
Cajo ter Braak <cajo.terbraak@wur.nl>
2024-05-23 15:54:03 +0200

    Add files via upload

--- demo/dune_fCWMSNC.r
+++ demo/dune_fCWMSNC.r
@@ -1,7 +1,8 @@
+library(douconca)
 data("dune_trait_env")
 # rownames are carried forward in results
 rownames(dune_trait_env$comm) <- dune_trait_env$comm$Sites
-divide = FALSE
+divide = TRUE
 mod1 <- dc_CA(formulaEnv = ~A1+Moist+Manure+Use+Condition(Mag),
               formulaTraits = ~ SLA + Height + LDMC + Condition(Seedmass) +Lifespan,
               response = dune_trait_env$comm[, -1],  # must delete "Sites"
@@ -31,14 +32,13 @@ CWMSNCb <- fCWM_SNC(formulaEnv = mod1$formulaEnv,
 names(CWMSNCb)
 
 mod2 <- dc_CA(formulaEnv =mod1$formulaEnv,
-               formulaTraits = mod1$formulaTraits,
-               response = CWMSNCa,  # included dataEnv, dataTraits
-               verbose = TRUE)
+              response = CWMSNCa,  # included dataEnv, dataTraits
+              verbose = TRUE)
 #Note: the axes of mod2 may have switched sign compared to mod1
+# formulaTrait is taken from CWMSNCa
 CWMSNCc <- CWMSNCa
 CWMSNCc$SNC <- NULL
 mod3 <- dc_CA(formulaEnv =mod1$formulaEnv,
-              formulaTraits = mod1$formulaTraits,
               response = CWMSNCc,  # included dataEnv, dataTraits
               verbose = TRUE)
 cbind(eig_mod1= mod1$eigenvalues, eig_mod2 = mod2$eigenvalues, eig_mod3= mod3$eigenvalues)
@@ -48,3 +48,32 @@ douconca::scores(mod2, display= type_of_score)/douconca::scores(mod1, display= t
 type_of_score <- "species"
 douconca::scores(mod2, display= type_of_score)/douconca::scores(mod1, display= type_of_score)
 #Note: the axes of mod2 have switched sign compared to mod1
+
+
+# example of no weights specified;
+CWMSNCd <-CWMSNCb
+CWMSNCd$weights <- NULL
+mod4 <- dc_CA(formulaEnv =mod1$formulaEnv,
+              response = CWMSNCd,  # included dataEnv, dataTraits
+              verbose = FALSE)
+# the analysis is not a real dc-CA:
+# the eigenvalues of CWM and SNC analyses differ, see above warning
+# the eigenvalues of the CWM analyses are not those of mod1
+mod4$eigenvalues/mod1$eigenvalues
+
+# and
+
+
+
+
+CWMSNCe <-CWMSNCd
+CWMSNCd$weights <- NULL
+# example of weights specified via dataTraits and dataEnv
+CWMSNCe$dataTraits <- dune_trait_env$traits
+CWMSNCe$dataTraits$weight <- CWMSNCa$weights$columns
+dune_trait_env$envir$weight <- CWMSNCa$weights$rows
+mod5 <- dc_CA(formulaEnv =mod1$formulaEnv,
+              dataEnv = dune_trait_env$envir,
+              response = CWMSNCe,  # included dataEnv, dataTraits
+              verbose = TRUE)
+mod5$eigenvalues/mod1$eigenvalues # mod5 is a proper dc-CA
