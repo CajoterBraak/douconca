128ab341f70aa1fd04297c5a7ea0acddd9e5a009
Cajo ter Braak <cajo.terbraak@wur.nl>
2024-05-25 16:41:06 +0200

    Add files via upload

--- R/predict.dcca.r
+++ R/predict.dcca.r
@@ -9,17 +9,27 @@
 #' \emph{i.e.} the fitted species niche centroids.
 #'
 #' @param object return value of \code{\link{dc_CA}}.
-#' @param type type of prediction, c("env", "traits","response","reg_env","reg_trait") for
+#' @param type type of prediction, c("env", "traits","response","reg_env","reg_trait", "response") for
 #' environmental values, values of traits,
 #' regression coefficients for environmenal and trait predictors.
-#' @param newdata An obtional data frame in which to look for variable with which to predict. If omitted,
-#' the fitted values are generated.
+#' @param newdata Data in which to look for variable with which to predict.
+#' For \code{type = "reg_env" or "reg_trait"} newdata is ignored.
+#' For \code{type = "env" or "trait"}, newdata is data frame of trait and environmental values,
+#' respectively, which are used for the prediction. If omitted, fitted values are generated.
+#' For \code{type = "response"}, newdata is a list of two data frames
+#' with trait and environmental values in this order,
+#' \emph{e.g.} list(traits = dataTraits, env = dataEnv).
 #' @param rank rank or number of axes to use. Default "full" for all axes (no rank-reduction).
 #' @details
 #' Variables that are in the model but not in \code{newdata} are set to their weighted means in
 #' the training data.
 #' Predictions are thus at the (weighted) mean of the quantitative variables not included.
 #' Predictions with not-included factors are at the weighted mean (none of the factor effects are included).
+#'
+#' For \code{type = "response"} and non-null newdata, the species weights of the training
+#' are used; the site weights are taken equal. Many of the predicted values may be negative,
+#' indicating expected absences (0) or small expected response values.
+#'
 #'
 #' @example demo/dune_dcCA_predict.R
 #' @export
@@ -30,29 +40,19 @@ predict.dcca <- function(object,
                          newdata=NULL){
   type <- match.arg(type)
   if (rank == "full") rank = length(object$eigenvalues)
-  if (type %in% c("traits","regr_env")) {
-    ff <- get_Z_X_XZ_formula(object$formulaEnv)
-    c_normed <- object$c_env_normed
-    if (is.null(newdata)) newdata <- object$data$dataEnv
+  if (type =="response") {
+    if (is.null(newdata)) newdata <-list(NULL,NULL)
+    newdata1 <- list(
+      traits = check_newdata(object, newdata[[1]], "env"), # env prediction requires trait data
+      env = check_newdata(object, newdata[[2]], "traits") #  trait prediction requires env data
+    )
   } else {
-    ff <- get_Z_X_XZ_formula(object$formulaTraits)
-    c_normed <- object$c_traits_normed
-    if (is.null(newdata)) newdata <- object$data$dataTraits
-  }
-
-  if (type %in% c("traits","env")) {
-      nams <- !ff$all_nams %in% names(newdata)
-      nams <-ff$all_nams[nams]
-      for (n in nams){
-          if (n %in% rownames(c_normed)) newdata[[n]] <- c_normed[n,"Avg"] # set to average
-          else newdata[[n]] <- 0 # for factors
-      }
-      newdata1 <- stats::model.matrix(ff$formula_XZ, constrasts = FALSE, data = newdata)[,-1,drop=FALSE]
-
+    if (type %in% c("env", "traits")) newdata1 <- check_newdata(object, newdata, type)
   }
   ret <- switch(type,
                 env = predict_env(object, newdata1, rank),
-                traits = predict_traits(object,newdata=newdata1, rank),
+                traits = predict_traits(object, newdata1, rank),
+                response = predict_response(object, newdata1, rank),
                 reg_env = predict_regr_env(object, rank),
                 reg_traits = predict_regr_traits(object, rank)
 
--- R/predict_functions.r
+++ R/predict_functions.r
@@ -14,18 +14,11 @@ predict_regr_traits <- function(object, rank){
   return(reg)
 }
 
-predict_traits <- function(object,  newdata, rank){
-  # missing factors in newdata : no contribution of the missing factors
+predict_traits <- function(object, newdata1, rank){
+  # missing factors in newdata1 : no contribution of the missing factors
   reg <- predict_regr_env(object, rank)
-  nms <- rownames(reg)
-  nms <- nms[nms %in% colnames(newdata)] # still needed for missing factors; see predict.dcca.r
-  dat0 <- newdata[,nms, drop = FALSE]
-  dat <- scale_data(dat0, mean_sd = object$c_env_normed[,c(1,2)])
-  #pred_scaled <- dat %*% reg[colnames(dat),, drop = FALSE ]
-  # reg may not have the covariates (e.g. when dc-CA was started from CWM) and
-  # newdata may not have all predictors
-  nams <- intersect(colnames(dat), rownames(reg))
-  pred_scaled <- dat[, nams,drop = FALSE] %*% reg[nams,, drop = FALSE ]
+  c_normed <- scores(object, choices = seq_len(rank), display = c("reg"))
+  pred_scaled <- fpred_scaled(newdata1, reg, c_normed)
   gg <- get_Z_X_XZ_formula(object$formulaTraits)
   traits0 <- stats::model.matrix(gg$formula_X0, constrasts = FALSE, data = object$data$dataTraits)
   msd = mean_sd_w(traits0, w = object$weights$columns)
@@ -36,11 +29,11 @@ predict_traits <- function(object,  newdata, rank){
 
 }
 
-scale_data <- function(newdata, mean_sd){
-  # newdata and mean_sd should  be matrices
-  ones <- rep(1,nrow(newdata))
-  Xc <- newdata - ones %*% t(mean_sd[,1,drop=FALSE][colnames(newdata),,drop= FALSE])
-  Xc <- Xc / (ones %*% t(mean_sd[,2, drop =FALSE][colnames(newdata),,drop = FALSE]))
+scale_data <- function(newdata1, mean_sd){
+  # newdata1 and mean_sd should  be matrices
+  ones <- rep(1,nrow(newdata1))
+  Xc <- newdata1 - ones %*% t(mean_sd[,1,drop=FALSE][colnames(newdata1),,drop= FALSE])
+  Xc <- Xc / (ones %*% t(mean_sd[,2, drop =FALSE][colnames(newdata1),,drop = FALSE]))
   return(Xc)
 }
 
@@ -53,18 +46,39 @@ backscale_data <- function(pred_scaled, msd){
  return(pred_scaled)
 }
 
-predict_env <- function(object,  newdata, rank){
-  # missing factors in newdata : no contribution of the missing factors
+check_newdata <- function(object, newdata, type){
+  # check for 1 data frame (either env or traits)
+  # BEWARE,
+  # type is "traits" or "env"
+  # where check_newdata gives environmental data and trait data respectively
+
+  if (type %in% c("traits")) {
+    ff <- get_Z_X_XZ_formula(object$formulaEnv)
+    c_normed <- object$c_env_normed
+    if (is.null(newdata)) newdata1 <- object$data$dataEnv
+  } else if (type %in% c("env")){ # "env", "reg_traits"
+    ff <- get_Z_X_XZ_formula(object$formulaTraits)
+    c_normed <- object$c_traits_normed
+    if (is.null(newdata)) newdata1 <- object$data$dataTraits
+  }
+  if (!is.null(newdata)){
+    newdata1 <- newdata
+    nams <- !ff$all_nams %in% names(newdata1)
+    nams <-ff$all_nams[nams]
+    for (n in nams){
+      if (n %in% rownames(c_normed)) newdata1[[n]] <- c_normed[n,"Avg"] # set to average
+      else newdata1[[n]] <- 0 # for factors
+    }
+
+  }
+  newdata1 <- stats::model.matrix(ff$formula_XZ, constrasts = FALSE, data = newdata1)[,-1,drop=FALSE]
+  return(newdata1)
+}
+
+predict_env <- function(object,  newdata1, rank){
+  # missing factors in newdata1 : no contribution of the missing factors
   reg <- predict_regr_traits(object, rank)
-  nms <- rownames(reg)
-  nms <- nms[nms %in% colnames(newdata)] # still needed for missing factors; see predict.dcca.r
-  dat0 <- newdata[,nms, drop = FALSE]
-  dat <- scale_data(dat0, mean_sd = object$c_traits_normed[,c(1,2)])
-  #pred_scaled <- dat %*% reg[colnames(dat),, drop = FALSE ]
-  # reg may not have the covariates (e.g. when dc-CA was started from CWM) and
-  # newdata may not have all predictors
-  nams <- intersect(colnames(dat), rownames(reg))
-  pred_scaled <- dat[, nams,drop = FALSE] %*% reg[nams,, drop = FALSE ]
+  pred_scaled <- fpred_scaled(newdata1, reg, c_normed= object$c_traits_normed0)
   gg <- get_Z_X_XZ_formula(object$formulaEnv)
   env0 <- stats::model.matrix(gg$formula_X0, constrasts = FALSE, data = object$data$dataEnv)
   msd = mean_sd_w(env0, w = object$weights$rows)
@@ -74,3 +88,29 @@ predict_env <- function(object,  newdata, rank){
   return(pred)
 
 }
+
+fpred_scaled <- function(newdata1, reg, c_normed){
+  #reg <- predict_regr_traits(object, rank)
+  nms <- rownames(reg)
+  nms <- nms[nms %in% colnames(newdata1)] # still needed for missing factors; see predict.dcca.r
+  dat0 <- newdata1[,nms, drop = FALSE]
+  dat <- scale_data(dat0, mean_sd = c_normed[,c(1,2)])
+  # reg may not have the covariates (e.g. when dc-CA was started from CWM) and
+  # newdata1 may not have all predictors
+  nams <- intersect(colnames(dat), rownames(reg))
+  pred_scaled <- dat[, nams,drop = FALSE] %*% reg[nams,, drop = FALSE ]
+  return(pred_scaled)
+}
+
+predict_response <- function(object, newdata1, rank){
+  # newdata1 must be a list two dataframes, element 1: trait and  element 2 env data
+  B_traits_regr <- scores(object, choices = seq_len(rank), display = c("reg_traits"), scaling ="sites")
+  pred_scaled_species <- fpred_scaled(newdata1[[1]], B_traits_regr[,-c(1,2,3), drop = FALSE], object$c_traits_normed0)
+  B_env_regr <- scores(object, choices = seq_len(rank), display = c("reg"), scaling ="sites")
+  pred_scaled_sites <- fpred_scaled(newdata1[[2]], B_env_regr[,-c(1,2,3), drop = FALSE], B_env_regr)
+  interact <- pred_scaled_sites %*% t(pred_scaled_species)
+  pred <- (1+interact) * rep(1, rep(nrow(interact)))%*% t(object$weights$columns)
+  return(pred)
+}
+
+
