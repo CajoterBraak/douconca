bd3dcec599977ab1221e669d52b10c81b41b96ae
Cajo ter Braak <cajo.terbraak@wur.nl>
2024-05-30 08:09:11 +0200

    Add files via upload

---
+++ demo/dune_dcCA_predict.r
@@ -0,0 +1,52 @@
+data("dune_trait_env")
+# rownames are carried forward in results
+rownames(dune_trait_env$comm) <- dune_trait_env$comm$Sites
+divide <- TRUE # divide by site.totals if TRUE
+
+
+cat("\n\n\n******* divide.by.site.totals ==", divide, "*******\n\n\n")
+mod <- dc_CA(formulaEnv = ~A1+Moist+Mag+Use+Condition(Manure),
+                   formulaTraits = ~ SLA + Height + LDMC + Condition(Seedmass) +Lifespan,
+                   response = dune_trait_env$comm[, -1],  # must delete "Sites"
+                   dataEnv =dune_trait_env$envir,
+                   dataTraits =dune_trait_env$traits,
+                   divide.by.site.totals = divide,
+                   verbose = TRUE)
+# regression coefficients
+predict(mod,type= c("reg_env"))
+predict(mod,type= c("reg_traits"))
+# fit the mean traits at each site (20x6),
+# that is CWM at each site, so analyse with CWMSNC version
+pred.traits<- predict(mod,type = c( "traits"))
+head(pred.traits)
+# fit the mean environment for each species (28x8)
+# that is SNC of each species
+pred.env<- predict(mod,type = c( "env"))
+head(pred.env)
+
+CWMSNC <- list(CWM = pred.traits, SNC = pred.env, weights= mod$weights, data = mod$data,
+               formulaEnv = mod$formulaEnv, formulaTraits = mod$formulaTraits)
+CWMSNC$data$Y <- NULL
+
+mod2 <- dc_CA(response = CWMSNC,
+             divide.by.site.totals = divide,
+             verbose = TRUE)
+
+
+mod$eigenvalues/mod2$eigenvalues
+mod2$site_axes$R2_env # perfect fit
+mod2$species_axes$R2_traits # perfect fit
+
+pred <- predict(mod,type= "response")
+# pred has negative values and dc_CA cannot have negatives in the response
+pred[pred<0] <-0
+
+mod3 <- dc_CA(formulaEnv = mod$formulaEnv,
+              formulaTraits = mod$formulaTraits,
+              response = pred,  # must delete "Sites"
+              dataEnv =dune_trait_env$envir,
+              dataTraits =dune_trait_env$traits,
+              divide.by.site.totals = divide,
+              verbose = TRUE)
+mod3$eigenvalues/mod$eigenvalues
+
---
+++ demo/dune_fCWMSNC.r
@@ -0,0 +1,89 @@
+library(douconca)
+data("dune_trait_env")
+# rownames are carried forward in results
+rownames(dune_trait_env$comm) <- dune_trait_env$comm$Sites
+divide = TRUE
+mod1 <- dc_CA(formulaEnv = ~A1+Moist+Manure+Use+Condition(Mag),
+              formulaTraits = ~ SLA + Height + LDMC + Condition(Seedmass) +Lifespan,
+              response = dune_trait_env$comm[, -1],  # must delete "Sites"
+              dataEnv =dune_trait_env$envir,
+              dataTraits =dune_trait_env$traits,
+              divide.by.site.totals = divide,
+              verbose = TRUE)
+
+
+CWMSNCa <- fCWM_SNC(formulaEnv = mod1$formulaEnv,
+                          formulaTraits = mod1$formulaTraits,
+                          response = dune_trait_env$comm[, -1],  # must delete "Sites"
+                          dataEnv =dune_trait_env$envir,
+                          dataTraits =dune_trait_env$traits,
+                          divide.by.site.totals = divide,
+                          verbose = FALSE)
+names(CWMSNCa)
+
+CWMSNCb <- fCWM_SNC(formulaEnv = mod1$formulaEnv,
+                          formulaTraits = mod1$formulaTraits,
+                          response = dune_trait_env$comm[, -1],  # must delete "Sites"
+                          dataEnv =dune_trait_env$envir,
+                          dataTraits =dune_trait_env$traits,
+                          divide.by.site.totals = divide,
+                          minimal_output = FALSE,
+                          verbose = FALSE)
+names(CWMSNCb)
+
+mod2 <- dc_CA(formulaEnv =mod1$formulaEnv,
+              response = CWMSNCa,  # included dataEnv, dataTraits
+              verbose = TRUE)
+#Note: the axes of mod2 may have switched sign compared to mod1
+# formulaTrait is taken from CWMSNCa
+CWMSNCc <- CWMSNCa
+CWMSNCc$SNC <- NULL
+mod3 <- dc_CA(formulaEnv =mod1$formulaEnv,
+              response = CWMSNCc,  # included dataEnv, dataTraits
+              verbose = TRUE)
+cbind(eig_mod1= mod1$eigenvalues, eig_mod2 = mod2$eigenvalues, eig_mod3= mod3$eigenvalues)
+mod2$eigenvalues/ mod1$eigenvalues
+type_of_score <- "sites"
+douconca::scores(mod2, display= type_of_score)/douconca::scores(mod1, display= type_of_score)
+type_of_score <- "species"
+douconca::scores(mod2, display= type_of_score)/douconca::scores(mod1, display= type_of_score)
+#Note: the axes of mod2 have switched sign compared to mod1
+
+
+# example of no weights specified;
+CWMSNCd <-CWMSNCb
+CWMSNCd$weights <- NULL
+mod4 <- dc_CA(formulaEnv =mod1$formulaEnv,
+              response = CWMSNCd,  # included dataEnv, dataTraits
+              verbose = FALSE)
+# the analysis is not a real dc-CA:
+# the eigenvalues of CWM and SNC analyses differ, see above warning
+# the eigenvalues of the CWM analyses are not those of mod1
+mod4$eigenvalues/mod1$eigenvalues
+
+
+CWMSNCe <-CWMSNCd
+CWMSNCe$weights <- NULL
+# example of weights specified via dataTraits and dataEnv
+CWMSNCe$dataTraits <- dune_trait_env$traits
+CWMSNCe$dataTraits$weight <- CWMSNCa$weights$columns
+dune_trait_env$envir$weight <- CWMSNCa$weights$rows
+mod5 <- dc_CA(formulaEnv =mod1$formulaEnv,
+              dataEnv = dune_trait_env$envir,
+              response = CWMSNCe,  # included dataEnv, dataTraits
+              verbose = TRUE)
+mod5$eigenvalues/mod1$eigenvalues # mod5 is a proper dc-CA
+
+# A minimal specification with a non-trivial trait model, giving 3 warnings
+dune_trait_env$envir$weight <- NULL
+CWMSNCf <- list(CWM = as.data.frame(CWMSNCa$CWM),
+                weights= list(columns = 100*mod1$weights$columns),
+                dataTraits =dune_trait_env$traits,
+                formulaTraits = ~ SLA + Height + LDMC + Condition(Seedmass) +Lifespan)
+# Without trait covariates and only traits in dataTraits, formulaTraits can be deleted from the list.
+# For the trait model, different environmental predictors can be used
+mod6 <- dc_CA(response =CWMSNCf,
+                dataEnv =dune_trait_env$envir,
+                formulaEnv = ~ Moist,
+                verbose = TRUE)
+
